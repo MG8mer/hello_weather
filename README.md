591d6dae0954c940b2460d67e2f058cde828859c
MG8mer <mhammouri.26@acsamman.edu.jo>
2023-05-10 22:38:16 +0300

    Added more to api.js + Started work on HTML/CSS.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# final_project_tbd
-TBD...
+# hello_weather!
+Have you gotten sick of the weather app on your phone? Well, this web application utilizes a weather API that tracks weather in realtime, and guess what, it will track the weather of any location you choose! Try it out ;)
--- api.js
+++ api.js
@@ -1,25 +1,28 @@
 /*
-Below code from https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys:
+Below code from 
+1. https://coding-boot-cap.github.io/full-stack/apis/how-to-use-api-keys 
+2. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+3. My Dad :)
 */
 var Data;
 async function logJSONData(url) {
     const response = await fetch(url);
     const jsonData = await response.json();
     Data = jsonData;
-    document.getElementById("output").innerHTML = Data['main']['temp'];
+    document.getElementById("temp").innerHTML = "Temp " + Number(Data['main']['temp']-273.15);
+    document.getElementById("output").innerHTML = "Feels Like " + Number(Data['main']['feels_like']-273.15);
 }
 
+const APIKey = "ed0e35b9304aa2a810b22c9bc7a56b60"
 
-var APIKey = "ed0e35b9304aa2a810b22c9bc7a56b60";
 
 var city = "amman";
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-logJSONData(queryURL)/*
-Above code from https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys:
+logJSONData(queryURL);
+/*
+Above code from 
+1. https://coding-boot-cap.github.io/full-stack/apis/how-to-use-api-keys 
+2. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+3. My Dad :)
 */
-
-
-
-// http://api.openweathermap.org/data/2.5/weather?q=amman&appid=ed0e35b9304aa2a810b22c9bc7a56b60
