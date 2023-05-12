712a0ed6776b3e7cebda1e3c674f393e042b407b
MG8mer <mhammouri.26@acsamman.edu.jo>
2023-05-12 14:53:09 +0300

    Modified algorithims (user input/geocodingAPI)

--- api.js
+++ api.js
@@ -2,9 +2,15 @@
 Below code partially from 
 1. https://coding-boot-cap.github.io/full-stack/apis/how-to-use-api-keys 
 2. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-3. My Dad :)
+3. https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt1
+4. https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API & https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/clearWatch
+5. My Dad :)
 */
-// Below code from ChatGPT https://chat.openai.com/
+
+// ChatGPT forgot to define the APIKey, outside the function, so the "interactiveWeatherQuery stopped working". I defined APIKey first, which allowed both functions to work.
+const APIKey = "ed0e35b9304aa2a810b22c9bc7a56b60"
+
+// Below code inspired by: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API & https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/clearWatch
 var watchID;
 function getPositionSuccess(position) {
     var lat = position.coords.latitude;
@@ -12,22 +18,31 @@ function getPositionSuccess(position) {
     navigator.geolocation.clearWatch(watchID)
     document.getElementById('lat').innerHTML = lat;
     document.getElementById('long').innerHTML = long;
+  
+// Below code from ChatGPT https://chat.openai.com *BUT* has been slightly modified by myself.
 const apiKEY = '161a25f78acb4ba295beb5b75f8d77c2';
 const geocodingAPI = "https://api.geoapify.com/v1/geocode/reverse?lat=" + lat +"&lon=" + long +"&apiKey=" + apiKEY;
+
 
 fetch(geocodingAPI)
   .then(response => response.json())
   .then(data => {
     const geoCity = data.features[0].properties.state || 'N/A';
     document.getElementById('geoCity').innerHTML = geoCity;
+
+    // Displays weather info for current location, can be changed by just enetering a new city in text box.
+    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + geoCity + "&appid=" + APIKey;
+    logJSONData(queryURL);
+    // ChatGPT helped me debug why this wasn't working when it was outside the getPositionSuccess function. It 
   })
   .catch(error => {
     console.log('Error occurred during reverse geocoding');
     document.getElementById('geoCity').innerHTML = 'N/A';
   });
 }
-// Above code from ChatGPT https://chat.openai.com/
+// Above code from ChatGPT https://chat.openai.com *BUT has been slightly modified by myself.
 
+// Below code inspired by: https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API & https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/clearWatch
 function getPositionDenied() {
     alert("Access to location denied by user.");
     document.getElementById('lat').innerHTML = "N/A";
@@ -42,7 +57,7 @@ function getPositionDenied() {
   
 var watchID = navigator.geolocation.watchPosition(getPositionSuccess, getPositionDenied, options);
 
-const APIKey = "ed0e35b9304aa2a810b22c9bc7a56b60"
+// Below Code Partially from https://coding-boot-cap.github.io/full-stack/apis/how-to-use-api-keys, https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch, and My Dad; what my Dad wrote has been modifified and added to greatly.
 var Data;
 async function logJSONData(url) {
     const response = await fetch(url);
@@ -59,29 +74,18 @@ async function logJSONData(url) {
     document.getElementById("wind").innerHTML = "Wind Speed: " + Number(Data["wind"]["speed"]) + "m/s";
 }
 
-const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + geoCity + "&appid=" + APIKey;
-logJSONData(queryURL)
+// Below Code Inspired By: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt1
+function weatherQuery() {
+  let city = window.prompt("What is the weather today?")
+  let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  logJSONData(queryURL);
+}
 /*
 Above code partially from 
 1. https://coding-boot-cap.github.io/full-stack/apis/how-to-use-api-keys 
 2. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-3. My Dad :)
+3. https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt1
+4. https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API & https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/clearWatch
+5. My Dad :)
 */
 
-
-
-
-// Below code from ChatGPT https://chat.openai.com/
-function interactiveWeatherQurty(event) {
-    event.preventDefault();
-    const weatherQuery = document.getElementById
-    ("weatherQuery");
-    const city = weatherQuery.value.trim();
-    if (city !== "") {
-    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    logJSONData(queryURL)
-    }
-}
-const formCity = document.getElementById("formCity");
-formCity.addEventListener("submit", interactiveWeatherQurty);
-// Above code from ChatGPT https://chat.openai.com/
